c93cfb462bc1d7f9d5d3d413bde558c7b897202a
Хван Дмитрий Максимович <drestbm@gmail.com>
2020-06-25 17:49:40 +0700

    skills

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,5 @@ import fonts from '@assets/fonts/fonts.css';
 import header from '@styles/header.sass';
 import layout from '@styles/layout.sass';
 import title from '@styles/title.sass';
+import section__title from '@styles/section__title.sass';
+import skills from '@styles/skills.sass';
